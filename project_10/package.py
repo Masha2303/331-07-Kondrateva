c149afc33c67fd334fe9fab2fef247a62f534d18
Masha <113627553+Masha2303@users.noreply.github.com>
2025-06-17 17:44:15 +0300

    Add files via upload

---
+++ project_10/package.py
@@ -0,0 +1,9 @@
+# requirements for module_03_kondr
+REQUIREMENTS = [
+    'Flask==3.0.0',
+    'SQLAlchemy==2.0.23',
+    'Flask-SQLAlchemy==3.1.1',
+    'python-dotenv==1.0.0',
+    'pandas==2.1.3',
+    'openpyxl==3.1.2',
+]
